2bf70088286c28a83e482d69b14e6007bb062d98
Louis <vundaboy@gmail.com>
2021-09-03 11:32:51 +0200

    order & limit query

--- core/DatabaseQuery.php
+++ core/DatabaseQuery.php
@@ -9,6 +9,8 @@ class DatabaseQuery {
   private $where = [];
   private $table = null;
   private $pdo = null;
+  private $limit = null;
+  private $orderBY = null;
 
   public function __construct($table) {
     $this->table = $table;
@@ -30,6 +32,14 @@ class DatabaseQuery {
       $sql .= $this->buildSearchQuery();
     }
 
+    if(!empty($this->orderBy)){
+      $sql .= $this->buildOrderQuery();
+    }
+
+    if($this->limit){
+      $sql .= $this->buildLimitQuery();
+    }
+
     $stmt = $this->pdo->prepare($sql. ';');
 
     foreach ($this->where as $column => $data) {
@@ -40,6 +50,26 @@ class DatabaseQuery {
     $res = $stmt->fetchAll();
 
     return BaseModel::parseResult($res);
+  }
+
+  private function buildOrderQuery(){
+    $sql = '';
+    $cpt = 0;
+    foreach ($this->orderBy as $column => $type) {
+      if($cpt===0){
+        $sql .=' ORDER BY ';
+      }
+      $sql .= $column . ' ' . $type ;
+      if($cpt+1!==count($this->orderBy)){
+        $sql .= ',';
+      }
+      $cpt++;
+    }
+    return $sql;
+  }
+
+  public function buildLimitQuery(){
+    return ' LIMIT ' . $this->limit;
   }
 
   private function buildSearchQuery() {
@@ -56,4 +86,14 @@ class DatabaseQuery {
     return $output;
   }
 
+  public function limit($lim){
+    $this->limit = $lim;
+
+    return $this;
+  }
+
+  public function orderBy($column, $type='ASC'){
+    $this->orderBy[$column] = $type;
+    return $this;
+  }
 }
